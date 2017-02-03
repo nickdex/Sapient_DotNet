e07644c5195b5775b42b3c50c561523e2d60b4a2
nickdex <nikwarke@gmail.com>
2017-02-03 12:22:20 +0530

    Files and Directory apis

--- FileIO/Form1.Designer.cs
+++ FileIO/Form1.Designer.cs
@@ -34,6 +34,12 @@
             this.btnLoad = new System.Windows.Forms.Button();
             this.txtBoxContent = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.lblFolderPath = new System.Windows.Forms.Label();
+            this.btnFolderBrowse = new System.Windows.Forms.Button();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.btnMisc = new System.Windows.Forms.Button();
+            this.lblFileInfo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnBrowse
@@ -77,12 +83,12 @@
             this.txtBoxContent.Location = new System.Drawing.Point(15, 56);
             this.txtBoxContent.Multiline = true;
             this.txtBoxContent.Name = "txtBoxContent";
-            this.txtBoxContent.Size = new System.Drawing.Size(362, 261);
+            this.txtBoxContent.Size = new System.Drawing.Size(125, 261);
             this.txtBoxContent.TabIndex = 4;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(383, 184);
+            this.btnSave.Location = new System.Drawing.Point(146, 248);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 23);
             this.btnSave.TabIndex = 5;
@@ -90,11 +96,71 @@
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Location = new System.Drawing.Point(203, 56);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(205, 20);
+            this.txtFolderPath.TabIndex = 6;
+            // 
+            // lblFolderPath
+            // 
+            this.lblFolderPath.AutoSize = true;
+            this.lblFolderPath.Location = new System.Drawing.Point(168, 56);
+            this.lblFolderPath.Name = "lblFolderPath";
+            this.lblFolderPath.Size = new System.Drawing.Size(61, 13);
+            this.lblFolderPath.TabIndex = 7;
+            this.lblFolderPath.Text = "Folder Path";
+            // 
+            // btnFolderBrowse
+            // 
+            this.btnFolderBrowse.Location = new System.Drawing.Point(414, 56);
+            this.btnFolderBrowse.Name = "btnFolderBrowse";
+            this.btnFolderBrowse.Size = new System.Drawing.Size(99, 23);
+            this.btnFolderBrowse.TabIndex = 8;
+            this.btnFolderBrowse.Text = "Browse Folder";
+            this.btnFolderBrowse.UseVisualStyleBackColor = true;
+            this.btnFolderBrowse.Click += new System.EventHandler(this.btnFolderBrowse_Click);
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.Location = new System.Drawing.Point(203, 98);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(205, 95);
+            this.listBoxFiles.TabIndex = 9;
+            this.listBoxFiles.SelectedIndexChanged += new System.EventHandler(this.listBoxFiles_SelectedIndexChanged);
+            // 
+            // btnMisc
+            // 
+            this.btnMisc.Location = new System.Drawing.Point(414, 156);
+            this.btnMisc.Name = "btnMisc";
+            this.btnMisc.Size = new System.Drawing.Size(75, 23);
+            this.btnMisc.TabIndex = 10;
+            this.btnMisc.Text = "Misc";
+            this.btnMisc.UseVisualStyleBackColor = true;
+            this.btnMisc.Click += new System.EventHandler(this.btnMisc_Click);
+            // 
+            // lblFileInfo
+            // 
+            this.lblFileInfo.AutoSize = true;
+            this.lblFileInfo.Location = new System.Drawing.Point(200, 206);
+            this.lblFileInfo.Name = "lblFileInfo";
+            this.lblFileInfo.Size = new System.Drawing.Size(51, 13);
+            this.lblFileInfo.TabIndex = 11;
+            this.lblFileInfo.Text = "lblFileInfo";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(560, 338);
+            this.Controls.Add(this.lblFileInfo);
+            this.Controls.Add(this.btnMisc);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.btnFolderBrowse);
+            this.Controls.Add(this.lblFolderPath);
+            this.Controls.Add(this.txtFolderPath);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.txtBoxContent);
             this.Controls.Add(this.btnLoad);
@@ -116,6 +182,12 @@
         private System.Windows.Forms.Button btnLoad;
         private System.Windows.Forms.TextBox txtBoxContent;
         private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Label lblFolderPath;
+        private System.Windows.Forms.Button btnFolderBrowse;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Button btnMisc;
+        private System.Windows.Forms.Label lblFileInfo;
     }
 }
 
---
+++ FileIO/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileIO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtBoxPath.Text = dialog.FileName;
+            }
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            clearContents();
+
+            //UseFileStream_Read();
+            //UseStreamReader();
+            UseFileStream_ReadAsync();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // TODO
+            //UseFileStream_Write()
+            // UseStreamWriter();
+            UseFileStream_WriteAsync();
+        }
+
+        private void UseFileStream_Read()
+        {
+            using (FileStream fileStream = new FileStream(txtBoxPath.Text, FileMode.Open))
+            {
+                byte[] bytes = new byte[512];
+                while (fileStream.Read(bytes, 0, bytes.Count()) != 0)
+                {
+                    ASCIIEncoding encoder = new ASCIIEncoding();
+                    txtBoxContent.Text += encoder.GetString(bytes);
+                }
+            }
+        }
+
+        private void UseStreamReader()
+        {
+            StreamReader reader = new StreamReader(txtBoxPath.Text);
+            txtBoxContent.Text = reader.ReadToEnd();
+            reader.Close();
+            reader.Dispose();
+        }
+
+        private void clearContents()
+        {
+            txtBoxContent.Text = "";
+        }
+
+
+        private void UseFileStream_Write()
+        {
+
+            using (FileStream stream = new FileStream(txtBoxPath.Text, FileMode.Truncate))
+            {
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                byte[] bytes = encoder.GetBytes(txtBoxContent.Text);
+                stream.Write(bytes, 0, bytes.Count());
+            }
+
+        }
+
+        private void UseStreamWriter()
+        {
+            StreamWriter writer = new StreamWriter(txtBoxPath.Text);
+            writer.WriteLine(txtBoxContent.Text);
+            writer.Close();
+            writer.Dispose();
+        }
+
+        private async void UseFileStream_ReadAsync()
+        {
+
+            using (FileStream fileStream = new FileStream(txtBoxPath.Text, FileMode.Open))
+            {
+                byte[] bytes = new byte[512];
+                while (await fileStream.ReadAsync(bytes, 0, bytes.Count()) != 0)
+                {
+                    ASCIIEncoding encoder = new ASCIIEncoding();
+                    txtBoxContent.Text += encoder.GetString(bytes);
+                }
+            }
+
+        }
+
+        private void UseFileStream_WriteAsync()
+        {
+
+            using (FileStream stream = new FileStream(txtBoxPath.Text, FileMode.Truncate))
+            {
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                byte[] bytes = encoder.GetBytes(txtBoxContent.Text);
+                stream.WriteAsync(bytes, 0, bytes.Count());
+            }
+
+        }
+
+        private void btnFolderBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFolderPath.Text = folderDialog.SelectedPath;
+            }
+
+
+        }
+
+        private void btnMisc_Click(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFiles(txtFolderPath.Text);
+
+            foreach (var item in files)
+            {
+                listBoxFiles.Items.Add(item);
+            }
+            
+        }
+
+        private void listBoxFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxFiles.SelectedIndex == -1) return;
+
+            FileInfo fileInfo = new FileInfo(listBoxFiles.SelectedItem as string);
+            lblFileInfo.Text = fileInfo.LastAccessTime.ToString();
+        }
+
+
+
+
+    }
+}
