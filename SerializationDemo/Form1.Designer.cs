113651e1942fbe1a0b6eab897ff9a584679dcc96
nickdex <nikwarke@gmail.com>
2017-02-06 10:34:39 +0530

    Serialization Demo v2

---
+++ SerializationDemo/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SerializationDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSerialize = new System.Windows.Forms.Button();
+            this.btnDeserialize = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtContents = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSerialize
+            // 
+            this.btnSerialize.Location = new System.Drawing.Point(236, 71);
+            this.btnSerialize.Name = "btnSerialize";
+            this.btnSerialize.Size = new System.Drawing.Size(75, 23);
+            this.btnSerialize.TabIndex = 0;
+            this.btnSerialize.Text = "Serialize";
+            this.btnSerialize.UseVisualStyleBackColor = true;
+            this.btnSerialize.Click += new System.EventHandler(this.btnSerialize_Click);
+            // 
+            // btnDeserialize
+            // 
+            this.btnDeserialize.Location = new System.Drawing.Point(236, 100);
+            this.btnDeserialize.Name = "btnDeserialize";
+            this.btnDeserialize.Size = new System.Drawing.Size(75, 23);
+            this.btnDeserialize.TabIndex = 1;
+            this.btnDeserialize.Text = "Deserialize";
+            this.btnDeserialize.UseVisualStyleBackColor = true;
+            this.btnDeserialize.Click += new System.EventHandler(this.btnDeserialize_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(130, 33);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(171, 20);
+            this.txtPath.TabIndex = 2;
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(67, 33);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(57, 13);
+            this.lblPath.TabIndex = 3;
+            this.lblPath.Text = "Store Path";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(307, 31);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtContents
+            // 
+            this.txtContents.Location = new System.Drawing.Point(92, 59);
+            this.txtContents.Multiline = true;
+            this.txtContents.Name = "txtContents";
+            this.txtContents.Size = new System.Drawing.Size(114, 73);
+            this.txtContents.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 305);
+            this.Controls.Add(this.txtContents);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnDeserialize);
+            this.Controls.Add(this.btnSerialize);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSerialize;
+        private System.Windows.Forms.Button btnDeserialize;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtContents;
+    }
+}
+
---
+++ SerializationDemo/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace SerializationDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSerialize_Click(object sender, EventArgs e)
+        {
+            BinaryFormatterWriter();
+            //XmlFormatterWriter();
+        }
+
+        private void btnDeserialize_Click(object sender, EventArgs e)
+        {
+            BinaryFormatterReader();
+           // XmlFormatterReader();
+        }
+
+        private void XmlFormatterWriter()
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(DemoClass));
+            FileStream stream = new FileStream("nikhil.txt", FileMode.Create);
+
+            DemoClass demo = new DemoClass(2);
+            demo.Id = 2134;
+            demo.Age = 12;
+            demo.Name = "Nikhil";
+
+            formatter.Serialize(stream, demo);
+            stream.Close();
+            stream.Dispose();
+        }
+
+        private void XmlFormatterReader()
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(DemoClass));
+            FileStream stream = new FileStream("nikhil.txt", FileMode.Open);
+            var obj = formatter.Deserialize(stream);
+
+            DemoClass demo = obj as DemoClass;
+            txtContents.Text = demo.ToString();
+            stream.Close();
+            stream.Dispose();
+        }
+
+        private static void BinaryFormatterWriter()
+        {
+            BinaryFormatter formater = new BinaryFormatter();
+
+            FileStream stream = new FileStream("nikhil.bak", FileMode.Create);
+
+            DemoClass demo = new DemoClass(2);
+            demo.Id = 2134;
+            demo.Age = 12;
+            demo.Name = "Nikhil";
+
+            formater.Serialize(stream, demo);
+            stream.Close();
+            stream.Dispose();
+        }
+
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void BinaryFormatterReader()
+        {
+            BinaryFormatter formater = new BinaryFormatter();
+            FileStream stream = new FileStream("nikhil.bak", FileMode.Open);
+            var obj = formater.Deserialize(stream);
+
+            DemoClass demo = obj as DemoClass;
+            txtContents.Text = demo.ToString();
+            stream.Close();
+            stream.Dispose();
+        }
+    }
+}
