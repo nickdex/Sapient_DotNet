71340b00a3d16a9ee0c699232b764eb68ca3d861
nickdex <nikwarke@gmail.com>
2017-02-08 10:03:51 +0530

    More on MVC - Post request

---
+++ MVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using MVCApplication.Framework;
+using MVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class HomeController : BaseController
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            string id = collection["UserId"];
+            string password = collection["Password"];
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            ViewBag.Message = "Message in Bag";
+            ViewData["blah"] = "Blah fromv view data";
+            Session["hello"] = "session message";
+            System.Web.HttpContext.Current.Cache["stuff"] = "Cache stuff";
+            return View("Index");
+        }
+
+        private byte[] GetBytes()
+        {
+            byte[] b = new byte[10];
+            return b;
+        }
+
+
+
+       
+    }
+}
