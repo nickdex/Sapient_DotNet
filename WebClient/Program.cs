6c3c4465287a0e3d7df10ea38270a4e6242e5b93
nickdex <nikwarke@gmail.com>
2017-02-21 12:34:17 +0530

    Mutiple Services

---
+++ WebClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculator.CalculatorClient client = new Calculator.CalculatorClient("Basic");
+            //Calculator.CoolerClient coolerClient = new Calculator.CoolerClient("Basic");
+            //var response = client.Operation(new Calculator.Request() { operation = Calculator.Operation.ADD, Value1 = 5, Value2 = 10 });
+
+            Cooler.CoolerClient client = new Cooler.CoolerClient("Basic1");
+
+            var result = client.CalculateCoolnes("nikhil");
+
+        }
+    }
+}
